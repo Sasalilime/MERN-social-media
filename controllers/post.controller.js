bbf54511709206515f3b19b3758abbc019590d1b
salim <salimtison@gmail.Com>
2020-12-02 15:56:58 +0100

    post controller without void

---
+++ controllers/post.controller.js
@@ -0,0 +1,20 @@
+const PostController = require('../models/post.model');
+const UserModel = require('../models/user.model');
+const ObjectId = require('mongoose').Types.ObjectId;
+
+
+module.exports.readPost = (req, res)  => {
+
+}
+
+module.exports.createPost = (req, res)  => {
+
+}
+
+module.exports.updatePost = (req, res)  => {
+
+}
+
+module.exports.deletePost = (req, res)  => {
+
+}

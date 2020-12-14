dc8927e5ea645b537a9bad3ca75e507ff0fbf193
salimtison <salimtison@gmail.com>
2020-12-14 13:56:36 +0100

    logout

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,13 @@
 import React, {useContext} from 'react';
 import {NavLink} from "react-router-dom";
 import {UserIdContext} from "./AppContext";
+import Logout from "./Log/Logout";
+import axios from "axios";
 
 const Navbar = () => {
     const userId = useContext(UserIdContext);
+
+
 
     return (
         <nav>
@@ -16,8 +20,29 @@ const Navbar = () => {
                         </div>
                     </NavLink>
                 </div>
+                {userId ? (<ul>
+                    <li>
+
+                    </li>
+                    <li className="welcome">
+                        <NavLink to="/profil">
+                            <h5>Bienvenue 'valeur dyn'</h5>
+                        </NavLink>
+                    </li>
+                    <Logout/>
+                </ul>) : (
+                    <ul>
+                        <li>
+
+                        </li>
+                        <li>
+                            <NavLink exact to="/profil">
+                                <img src="./img/icons/login.svg" alt="login"/>
+                            </NavLink>
+                        </li>
+                    </ul>
+                )}
             </div>
-            
         </nav>
     );
 };

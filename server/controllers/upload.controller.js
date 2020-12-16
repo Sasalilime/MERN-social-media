0dd1823b041fb989c4665e95c8fbb4bb605654d9
salim <salimtison@gmail.Com>
2020-12-16 13:51:59 +0100

    uploadpicture fixed

--- server/controllers/upload.controller.js
+++ server/controllers/upload.controller.js
@@ -1,8 +1,8 @@
 const UserModel = require("../models/user.model");
 const fs = require("fs");
-const { promisify } = require("util");
+const {promisify} = require("util");
 const pipeline = promisify(require("stream").pipeline);
-const { uploadErrors } = require("../utils/errors.utils");
+const {uploadErrors} = require("../utils/errors.utils");
 
 module.exports.uploadProfil = async (req, res) => {
     try {
@@ -17,30 +17,30 @@ module.exports.uploadProfil = async (req, res) => {
             throw Error("max size");
     } catch (err) {
         const errors = uploadErrors(err);
-        return res.status(201).json({ errors });
+        return res.status(201).json({errors});
     }
     const fileName = req.body.name + ".jpg";
 
     await pipeline(
         req.file.stream,
         fs.createWriteStream(
-            `${__dirname}/../client/public/uploads/profil/${fileName}`
+            `${__dirname}/../../client/public/uploads/profil/${fileName}`
         )
     );
 
     try {
         await UserModel.findByIdAndUpdate(
             req.body.userId,
-            { $set : {picture: "./uploads/profil/" + fileName}},
-            { new: true, upsert: true, setDefaultsOnInsert: true},
+            {$set: {picture: "./uploads/profil/" + fileName}},
+            {new: true, upsert: true, setDefaultsOnInsert: true},
             (err, docs) => {
                 if (!err)
                     return res.send(docs);
                 else
-                    return res.status(500).send({ message: err });
+                    return res.status(500).send({message: err});
             }
         );
     } catch (err) {
-        return res.status(500).send({ message: err });
+        return res.status(500).send({message: err});
     }
 };

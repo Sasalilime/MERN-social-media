0112ecc639510aab25f2bb5fe5ba1879f613e672
salimtison <salimtison@gmail.com>
2020-12-29 12:36:51 +0100

    6:03:09
    Crud

--- client/src/components/Post/LikeButton.js
+++ client/src/components/Post/LikeButton.js
@@ -2,25 +2,30 @@ import React, {useState, useContext, useEffect} from 'react';
 import {UserIdContext} from "../AppContext";
 import Popup from "reactjs-popup";
 import "reactjs-popup/dist/index.css";
+import {useDispatch} from "react-redux";
+import {likePost, unlikePost} from "../../actions/post.actions";
 
 const LikeButton = ({post}) => {
     const [liked, setLiked] = useState(false);
     const userId = useContext(UserIdContext);
+    const dispatch = useDispatch();
 
     const like = () => {
-        // dispatch(likePost(post._id, userId))
-        // setLiked(true);
+        dispatch(likePost(post._id, userId));
+        setLiked(true);
     };
 
     const unlike = () => {
-        // dispatch(unlikePost(post._id, userId))
-        // setLiked(false);
+        dispatch(unlikePost(post._id, userId))
+        setLiked(false);
     };
 
 
     useEffect(() => {
         if (post.likers.includes(userId))
             setLiked(true);
+        else
+            setLiked(false);
     }, [userId, post.likers, liked]);
 
 
--- client/src/components/Thread.js
+++ client/src/components/Thread.js
@@ -6,15 +6,29 @@ import Card from "./Post/Card";
 
 const Thread = () => {
     const [loadPosts, setLoadPosts] = useState(true);
+    const [count, setCount] = useState(5);
     const dispatch = useDispatch();
-    const posts = useSelector((state => state.postReducer))
+    const posts = useSelector((state => state.postReducer));
+
+    const loadMorePosts = () =>{
+//quand on arrive en bas de la page et que ça touche le bas(cf +1)  > tout ce qui est scrollable a
+        if (window.innerHeight + document.documentElement.scrollTop + 1 > document.scrollingElement.scrollHeight) {
+            setLoadPosts(true);
+        }
+    }
 
     useEffect(() => {
         if (loadPosts) {
-            dispatch(getPosts());
+            dispatch(getPosts(count));
             setLoadPosts(false);
+            setCount(count + 5);
         }
-    }, [loadPosts, dispatch]);
+
+
+//Avec React, il faut mettre l'événement puis l'enlever ensuite.
+        window.addEventListener('scroll', loadMorePosts);
+        return () => window.removeEventListener('scroll', loadMorePosts);
+    }, [loadPosts, dispatch, count]);
 
     return (
         <div className="thread-container">
